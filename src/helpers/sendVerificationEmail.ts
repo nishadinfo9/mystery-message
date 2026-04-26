8b69f806c5bf3bc18d3d2bf5aa93627e3fe48dd2
nishadinfo09 <mdnishadali03@gmail.com>
2026-04-26 23:43:57 +0600

    implement sendVerificationEmail function to send verification emails

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,28 @@
 import { resend } from "@/lib/resend";
 import verificationEmail from "@/email/verificationEmail";
+import { ApiResponse } from "@/types/apiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string,
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "@resend.dev",
+      to: email,
+      subject: "mystery message | verification code",
+      react: verificationEmail({ username, otp: verifyCode }),
+    });
+    return {
+      success: true,
+      message: "verification email send successfully",
+    };
+  } catch (emailError) {
+    console.log("Error sending verification email:", emailError);
+    return {
+      success: false,
+      message: "failed to send verification email",
+    };
+  }
+}
