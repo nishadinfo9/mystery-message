6ac53918137a8ae7acfa97d6184288843b5a7b8c
nishadinfo09 <mdnishadali03@gmail.com>
2026-05-03 21:53:53 +0600

    add API endpoint to retrieve user messages

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,48 @@
+import dbConnect from "../../../lib/db";
+import UserModel from "@/model/UserModel";
+import { authOptions } from "../auth/[...nextauth]/option";
+import { getServerSession } from "next-auth";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!session || !session.user) {
+    return Response.json(
+      { success: false, message: "Not Authenticated" },
+      { status: 401 },
+    );
+  }
+
+  const userId = new mongoose.Types.ObjectId(user?._id);
+
+  try {
+    const user = await UserModel.aggregate([
+      { $match: { id: userId } },
+      { $unwind: "$messages" },
+      { $sort: { "messages.createdAt": -1 } },
+      { $group: { _id: "$_id", messages: { $push: "$messages" } } },
+    ]);
+
+    if (!user || user.length === 0) {
+      return Response.json(
+        { success: false, message: "user not found" },
+        { status: 401 },
+      );
+    }
+
+    return Response.json(
+      { success: true, messages: user[0].message },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log("messager failed to get", error);
+    return Response.json(
+      { success: false, message: "messager failed to get" },
+      { status: 500 },
+    );
+  }
+}
